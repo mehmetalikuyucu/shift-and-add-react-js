8c0b796a8068a2bdaab05a364048fcd9ec26b5fa
Mehmet Ali Kuyucu <whoamay123@gmail.com>
2022-12-04 22:16:50 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
+function App () {
+  const [number1, setNumber1] = useState('')
+  const [number2, setNumber2] = useState('')
+  const [result, setResult] = useState('')
+  const [registers, setRegisters] = useState([])
+
+  const handleChange = event => {
+    if (event.target.name === 'number1') {
+      setNumber1(event.target.value)
+    } else if (event.target.name === 'number2') {
+      setNumber2(event.target.value)
+    }
+  }
+
+  const calculate = () => {
+    let num1 = parseInt(number1, 2)
+    let num2 = parseInt(number2, 2)
+
+    let result = 0
+    let registers = []
+
+    while (num2 > 0) {
+      if (num2 & 1) {
+        result += num1
+      }
+
+      registers.push(result.toString(2))
+
+      num1 <<= 1
+      num2 >>= 1
+    }
+
+    setResult(result.toString(2))
+    setRegisters(registers)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <label>çarpılan:</label><br></br>
+      <input
+        type='text'
+        name='number1'
+        value={number1}
+        onChange={handleChange}
+        placeholder='Binary number 1'
+      /><br></br>
+      <label>çarpan:</label>
+      <br></br>
+      
+      <input
+        type='text'
+        name='number2'
+        value={number2}
+        onChange={handleChange}
+        placeholder='Binary number 2'
+      />
+      <button onClick={calculate}>Calculate</button>
+      <p>Result: {result}</p>
+      <p>
+        her bir kaydırmada sonuç:{' '}
+        {registers.map(register => (
+          <span>{register},</span>
+        ))}
+      </p>
+      <div>
+        powered by Mehmet Ali Kuyucu with OpenAi ChatGPT
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
